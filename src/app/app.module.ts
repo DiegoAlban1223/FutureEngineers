53e8a84f067de91d2d028b3960b49897bdca7c03
DiegoAlban1223 <diegoalbanmoreno@gmail.com>
2023-04-11 17:40:27 -0500

    añadir list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { AngularMaterialModule } from './components/shared/angular-material/angular-material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
