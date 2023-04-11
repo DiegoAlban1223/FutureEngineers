44738b72a13ced96cdbfd409c5c77b15a0a3cfd2
DiegoAlban1223 <diegoalbanmoreno@gmail.com>
2023-04-11 17:47:39 -0500

    crear nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListStudentComponent } from './components/list-student/list-student.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListStudentComponent
+    ListStudentComponent,
+    AddStudentComponent
   ],
   imports: [
     BrowserModule,
