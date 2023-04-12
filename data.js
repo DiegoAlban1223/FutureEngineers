6f55b131428b92427e4684c64f468e4213504142
TadashiBD <tadashidulantobuitron@gmail.com>
2023-04-12 14:50:58 -0500

    no lineas de error, pero si en terminal
    Error: [BABEL] C:\Users\TADASHI\OneDrive - Universidad Peruana de Ciencias\Documents\GitHub\FutureEngineers\node_modules\@angular\material\fesm2020\table.mjs: Cannot find module 'C:\Users\TADASHI\OneDrive - Universidad Peruana de Ciencias\Documents\GitHub\FutureEngineers\node_modules\@babel\plugin-transform-named-capturing-groups-regex\lib\index.js'. Please verify that the package.json has a valid "main" entry (While processing: "base$0")

--- data.js
+++ data.js
@@ -4,28 +4,28 @@ module.exports = function () {
       {
         id:1,
         mensaje_estudiante: "buenas tardes profesora",
-	mensaje_tutor: "buenas tardes alumno",
+	      mensaje_tutor: "buenas tardes alumno",
         fecha_envio: "2023-04-12T12:27:45Z",
         fecha_recepcion: "2023-04-12T12:27:46Z",
       },
       {
         id:2,
         mensaje_estudiante: "Buenas tardes",
-	mensaje_tutor: "buenas tardes delegado",
+	      mensaje_tutor: "buenas tardes delegado",
         fecha_envio: "2023-04-12T12:28:40Z",
         fecha_recepcion: "2023-04-12T12:28:41Z",
       },
       {
         id:3,
         mensaje_estudiante: "le envie un correo ayer",
-	mensaje_tutor: "si me llego tu correo",
+	      mensaje_tutor: "si me llego tu correo",
         fecha_envio: "2023-04-12T12:30:23Z",
         fecha_recepcion: "2023-04-12T12:30:24Z",
       },
       {
         id:4,
         mensaje_estudiante: "estoy emocionado por los temas de hoy",
-	mensaje_tutor: "Exacto hoy la clase sera muy dinamica",
+	      mensaje_tutor: "Exacto hoy la clase sera muy dinamica",
         fecha_envio: "2023-04-12T12:32:12Z",
         fecha_recepcion: "2023-04-12T12:32:13Z",
 	}
@@ -34,7 +34,7 @@ module.exports = function () {
       {
         id: 1,
         nameBook: "Las leyes",
-	   nHojas: "145",
+	      nHojas: "145",
         publicationDateAuthor: "2022-09-09",
         author:
         {
