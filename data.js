a110a24ff7caddab2dd478a78ca6da7e8c63d300
TadashiBD <tadashidulantobuitron@gmail.com>
2023-04-12 22:22:26 -0500

    Formato arreglado Fechas
    Formato para fecha_ envio y fecha_recepcion

--- data.js
+++ data.js
@@ -5,29 +5,29 @@ module.exports = function () {
         id:1,
         mensaje_estudiante: "buenas tardes profesora",
 	      mensaje_tutor: "buenas tardes alumno",
-        fecha_envio: "2023-04-12T12:27:45Z",
-        fecha_recepcion: "2023-04-12T12:27:46Z",
+        fecha_envio: "2023-04-12T17:30:45.000Z",
+        fecha_recepcion: "2023-04-12T17:30:46.000Z",
       },
       {
         id:2,
         mensaje_estudiante: "Buenas tardes",
 	      mensaje_tutor: "buenas tardes delegado",
-        fecha_envio: "2023-04-12T12:28:40Z",
-        fecha_recepcion: "2023-04-12T12:28:41Z",
+        fecha_envio: "2023-04-12T17:28:40.000Z",
+        fecha_recepcion: "2023-04-12T17:28:41.000Z",
       },
       {
         id:3,
         mensaje_estudiante: "le envie un correo ayer",
 	      mensaje_tutor: "si me llego tu correo",
-        fecha_envio: "2023-04-12T12:30:23Z",
-        fecha_recepcion: "2023-04-12T12:30:24Z",
+        fecha_envio: "2023-04-12T17:30:23.000Z",
+        fecha_recepcion: "2023-04-12T17:30:24.000Z",
       },
       {
         id:4,
         mensaje_estudiante: "estoy emocionado por los temas de hoy",
 	      mensaje_tutor: "Exacto hoy la clase sera muy dinamica",
-        fecha_envio: "2023-04-12T12:32:12Z",
-        fecha_recepcion: "2023-04-12T12:32:13Z",
+        fecha_envio: "2023-04-12T17:32:12.000Z",
+        fecha_recepcion: "2023-04-12T17:32:13.000Z",
 	    }
     ]
   }
