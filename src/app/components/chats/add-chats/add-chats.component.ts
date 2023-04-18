15b9e83a9ea2b894d7dc397f39a8474c850670b7
TadashiBD <tadashidulantobuitron@gmail.com>
2023-04-18 02:06:40 -0500

    npm i @angular/material-moment-adapter --force
    forzando la instalacion de moment en add-chat.component.ts

--- src/app/components/chats/add-chats/add-chats.component.ts
+++ src/app/components/chats/add-chats/add-chats.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Chats } from 'src/app/model/Chats';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-add-chats',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-chats.component.css']
 })
 export class AddChatsComponent implements OnInit {
-
-  constructor() { }
+  form:FormGroup = new FormGroup({});
+  chats: Chats = new Chats();
+  mensaje:string = "";
+  maxFecha: Date = moment().add(-1, 'days').toDate();
 
   ngOnInit(): void {
   }
+
+  constructor() { }
+
 
 }
